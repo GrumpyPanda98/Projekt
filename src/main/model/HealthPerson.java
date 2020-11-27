0987a3c3bb876f398bce2998f20a1d8e84292919
Nickolaj <nickolaj.ajay@gmail.com>
2020-11-27 13:36:42 +0100

    Model update

---
+++ src/main/model/HealthPerson.java
@@ -0,0 +1,13 @@
+public class HealthPerson {
+    private String username;
+    private String password;
+    private String name;
+    private String workplace;
+
+public HealthPerson(String username, String password, String name, String workplace){
+    
+}
+
+
+}
+
