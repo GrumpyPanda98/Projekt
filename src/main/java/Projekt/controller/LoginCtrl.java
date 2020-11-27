d11e7b93a225edb3dd39a088c284762b7a8dca11
HClark97 <71780567+HClark97@users.noreply.github.com>
2020-11-27 14:49:49 +0100

    freaking freaks

--- src/main/java/Projekt/controller/LoginCtrl.java
+++ src/main/java/Projekt/controller/LoginCtrl.java
@@ -1,2 +1,21 @@
 package Projekt.controller;
 
+import javafx.fxml.FXML;
+import Projekt.MainApp;
+import Projekt.model.Healthperson;
+public class LoginCtrl{
+
+
+
+
+    public void VerifyUser(usernameText:Textfield,passwordText:PasswordField):{
+
+
+    }
+
+
+}
+
+
+
+
--- src/main/java/Projekt/model/datacategory.java
+++ src/main/java/Projekt/model/datacategory.java
@@ -1,6 +1,20 @@
 package Projekt.model;
+import java.time;
+
 
 public class Datacategory {
-private date date;
+    private localdate date;
+    private String doneBy;
+    private boolean graphValidy;
+    private String notes;
+
+    public Datacategory(localdate date, String doneBy, boolean graphValidy, String notes){
+        this.date = date;
+        this.doneBy = doneBy;
+        this.graphValidy = graphValidy;
+        this.notes = notes;
+    }
+
 
 }
+
