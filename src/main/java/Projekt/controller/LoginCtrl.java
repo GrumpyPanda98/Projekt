8e46c4d0b1eb5846cffef93c9119c18d2d863ff9
HClark97 <71780567+HClark97@users.noreply.github.com>
2020-11-30 10:20:03 +0100

    ok

--- src/main/java/Projekt/controller/LoginCtrl.java
+++ src/main/java/Projekt/controller/LoginCtrl.java
@@ -1,21 +1,20 @@
-/*package Projekt.controller;
+package Projekt.controller;
 
 import javafx.fxml.FXML;
 import Projekt.MainApp;
-import Projekt.model.Healthperson;
+// import Projekt.model.Healthperson;
 public class LoginCtrl{
 
 
 
-
+/*
     public void VerifyUser(usernameText:Textfield,passwordText:PasswordField) {
 
 
     }
 
-
-}
 */
+}
 
 
 
