b978dd2c9d045cf08ebe2ebd4c754f17f8cd56c5
Nickolaj <63777030+GrumpyPanda98@users.noreply.github.com>
2020-12-06 17:30:09 +0100

    Datekategori opdateret

---
+++ src/main/java/Projekt/controller/DatacategoryCtrl.java
@@ -0,0 +1,79 @@
+import Projekt.MainApp;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+public class DatacategoryCtrl {
+
+    @FXML
+    private Font x1;
+
+    @FXML
+    private Color x2;
+
+    @FXML
+    private Button smokestatus;
+
+    @FXML
+    private Button fev1;
+
+    @FXML
+    private Button mwt;
+
+    @FXML
+    private Button bmi;
+
+    @FXML
+    private Button fvc;
+
+    @FXML
+    private Button weight;
+
+    @FXML
+    private Button mrc;
+
+    @FXML
+    private TableView<?> results;
+
+    @FXML
+    private TextArea notes;
+
+    @FXML
+    void bmiPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fev1Pressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fvcPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mrcPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mwtPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void smokestatusPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void weightPressed(ActionEvent event) {
+
+    }
+
+}
